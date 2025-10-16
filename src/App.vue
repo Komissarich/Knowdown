283e36478647044c49db4ce0b346d6ff89751589
Komissarich <egorkart1@gmail.com>
2025-10-16 13:24:08 +0300

    added dockerfile for frontend

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
-
 <template>
   <VApp>
-  <Navbar></Navbar>
-  <VMain>
-    <RouterView v-slot="{ Component }">
+    <Navbar></Navbar>
+    <VMain>
+      <RouterView v-slot="{ Component }">
         <v-fade-transition hide-on-leave>
           <component :is="Component" />
         </v-fade-transition>
       </RouterView>
-  </VMain>
+    </VMain>
   </VApp>
-  
 </template>
 
 <script setup>
-  import Navbar from './components/misc/Navbar.vue';
-  
-  
+import Navbar from "./components/misc/Navbar.vue";
 </script>
