20d411fee693276656fe343cd4e7ac291919969f
Komissarich <egorkart1@gmail.com>
2025-12-19 01:42:47 +0300

    fixed websockets

--- lobby_service/src/main/java/com/example/lobby_service/Config/WebSocketConfig.java
+++ lobby_service/src/main/java/com/example/lobby_service/Config/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/")
-                .setAllowedOrigins("http://localhost:8080");
+                .setAllowedOrigins("http://87.228.89.76:8080");
 
     }
 
